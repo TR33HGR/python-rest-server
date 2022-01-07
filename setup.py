f7c92fb29991ff4508124a47253980f9ddde3b4e
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2022-01-07 15:48:41 +0900

    A simple Rest server printing hello world

--- setup.py
+++ setup.py
@@ -22,5 +22,8 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
+    install_requires=[
+          'flask',
+    ],
     python_requires=">=3.6",
 )
