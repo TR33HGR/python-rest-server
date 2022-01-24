104e1095f7a457e63d5dd4e37293decf876bd714
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2022-01-24 15:31:09 +0900

    Make login page

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setuptools.setup(
     packages=setuptools.find_packages(where="src"),
     install_requires=[
           'flask',
+          'flask-wtf',
           'python-dotenv',
     ],
     python_requires=">=3.6",
--- src/microblog/app/routes.py
+++ src/microblog/app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from microblog.app import app
+from microblog.app.forms import LoginForm
 
 
 @app.route('/')
@@ -17,3 +18,8 @@ def index():
         },
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
